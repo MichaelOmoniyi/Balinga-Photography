26aed53c76adf9ef35baea3f3bc9579d1238bd3c
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-03-14 06:57:20 +0100

    Links page

---
+++ balinga-photography/src/app/links/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Links from "../components/linksPage/Links";
+import "../components/linksPage/style.css"
+
+const page = () => {
+  return (
+    <div>
+      <Links />
+    </div>
+  );
+};
+
+export default page;
