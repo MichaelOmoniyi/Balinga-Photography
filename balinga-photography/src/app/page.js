c6e8190a37fb286367a381247c2ecca9afbcb6a1
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-03-14 06:56:44 +0100

    Main home page

---
+++ balinga-photography/src/app/page.js
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar";
+import Hero from "@/app/components/home/Hero";
+import About from "@/app/components/home/About";
+import Quote from "@/app/components/home/Quote";
+import Photos from "@/app/components/home/Photos";
+import Checkout from "@/app/components/home/Checkout";
+import Connect from "@/app/components/home/Connect";
+import Highlight from "@/app/components/home/Highlight";
+import Footer from "@/app/components/home/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Quote />
+      <Photos />
+      <Checkout />
+      <Connect />
+      <Highlight />
+      <Footer />
+    </div>
+  );
+}
