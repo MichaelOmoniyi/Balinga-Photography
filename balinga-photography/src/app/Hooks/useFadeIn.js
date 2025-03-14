5148fb8d1fa8c0d61619aa5be63ce84a265896b2
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-03-14 06:55:59 +0100

    Implementing hook to handle objects fading to the main page

---
+++ balinga-photography/src/app/Hooks/useFadeIn.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+
+const useFadeIn = (selectors) => {
+  useEffect(() => {
+    const elements = document.querySelectorAll(selectors);
+
+    if (!elements.length) return; // Avoid errors if elements are not found
+
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (
+            entry.isIntersecting &&
+            !entry.target.classList.contains("fade-in")
+          ) {
+            entry.target.classList.add("fade-in");
+            observer.unobserve(entry.target); // Stop observing once animated
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    elements.forEach((element) => observer.observe(element));
+
+    return () => observer.disconnect(); // Cleanup on unmount
+  }, []);
+};
+
+export default useFadeIn;
