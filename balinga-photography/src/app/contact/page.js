b022d18cbe91a69cecc3f06eaef4c66e9740eadf
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-03-23 05:46:07 +0100

    Implementing contact page

---
+++ balinga-photography/src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Contact from '@/app/components/contact/Contact'
+import '../components/contact/style.css'
+
+const page = () => {
+  return (
+    <div><Contact /></div>
+  )
+}
+
+export default page
