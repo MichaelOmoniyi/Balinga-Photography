509646dc6cc4d4166d5abee981ee5ced0a5452e0
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-03-27 21:32:50 +0100

    Implementing about page

---
+++ balinga-photography/src/app/about/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "../components/about/About";
+import "../components/about/style.css";
+import "../components/contact/style.css";
+
+const Page = () => {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+};
+
+export default Page;
