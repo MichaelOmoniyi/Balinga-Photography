2c9003ae2269ec77ef8aa09f131e6609d0e756c3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-03-28 06:08:00 +0100

    Implementing gallery containing mini-porfolio

--- balinga-photography/src/app/components/about/Gallery.jsx
+++ balinga-photography/src/app/components/about/Gallery.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import frameFocus from "@/app/Hooks/frameFocus";
 
 const Gallery = () => {
-  return (
-    <div></div>
-  )
-}
+    frameFocus(".frame")
 
-export default Gallery
+  return (
+    <div>
+      <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-1.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-2.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-3.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-4.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-5.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-6.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-7.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-8.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-9.jpg"
+            alt=""
+          />
+        </div>
+        <div className="frame">
+          <img
+            class="h-auto max-w-full rounded-lg"
+            src="/images/mini-portfolio/img-10.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
