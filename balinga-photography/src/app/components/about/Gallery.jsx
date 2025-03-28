81f2568e85f4f9e3a8f789652321fafc1c96a7a5
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-03-28 07:01:53 +0100

    Implementing zoom effect functionality

--- balinga-photography/src/app/components/about/Gallery.jsx
+++ balinga-photography/src/app/components/about/Gallery.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useFrameFocus from "@/app/Hooks/useFrameFocus";
+import useZoomEffect from "@/app/Hooks/useZoomEffect";
 
 const Gallery = () => {
   useFrameFocus(".frame");
@@ -7,70 +8,70 @@ const Gallery = () => {
   return (
     <div>
       <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-1.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-2.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-3.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-4.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-5.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-6.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-7.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-8.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-9.jpg"
             alt=""
           />
         </div>
-        <div className="frame">
+        <div className="frame transitionEffect scale-110 opacity-0">
           <img
             class="h-auto max-w-full rounded-lg"
             src="/images/mini-portfolio/img-10.jpg"
