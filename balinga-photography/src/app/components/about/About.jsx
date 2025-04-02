e6b59f6caf2234be9ea1bb3b3131cba0cdd76dfa
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-02 19:41:11 +0100

    Update About component padding

--- balinga-photography/src/app/components/about/About.jsx
+++ balinga-photography/src/app/components/about/About.jsx
@@ -68,7 +68,7 @@ const About = () => {
       <Hero />
       <div className="p-6 md:p-10">
         <Story />
-        <p className="transitionEffect font-serif font-bold text-xl text-black dark:text-white mt-6 px-40 scale-110 opacity-0">
+        <p className="transitionEffect font-serif font-bold text-xl text-black dark:text-white mt-6 px-5 md:px-20 lg:px-40 scale-110 opacity-0">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
           architecto illo, eaque rerum at impedit ratione cum, tempore in error
           officia unde id aspernatur dicta harum blanditiis sunt? Quia,
