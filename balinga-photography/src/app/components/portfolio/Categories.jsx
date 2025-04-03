6ed98c990c2eb36c6384008d4209c81fe4ec34b7
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-03 07:51:54 +0100

    Change active category border color

--- balinga-photography/src/app/components/portfolio/Categories.jsx
+++ balinga-photography/src/app/components/portfolio/Categories.jsx
@@ -1,21 +1,78 @@
-import React from "react";
-import data from "@/app/data/data";
+"use client";
+import React, { useEffect, useState } from "react";
+import ImageCategories from "./ImageCategories";
 
-const Categories = () => {
+const Categories = ({categoryType}) => {
+  const [category, setCategory] = useState(categoryType);
   return (
     <div>
       <header>
-        <div className="w-full overflow-hidden">
-          <div className="w-full flex gap-2 overflow-auto">
-            <span>All</span>
-            <span>Wedding</span>
-            <span>Traditional Wedding</span>
-            <span>Birthday</span>
-            <span>Convocation</span>
-            <span>Family</span>
+        <div className="w-full py-4 mb-4 overflow-hidden">
+          <div className="w-full flex gap-4 text-gray-700 dark:text-gray-500 overflow-auto">
+            <span
+              className={`${
+                category === "all"
+                  ? "border-gray-600 dark:border-gray-500"
+                  : null
+              } py-1 border-b-2 border-transparent hover:cursor-pointer flex items-center`}
+              onClick={() => setCategory("all")}
+            >
+              All
+            </span>
+            <span
+              className={`${
+                category === "wedding"
+                  ? "border-gray-600 dark:border-gray-500"
+                  : null
+              } py-1 border-b-2 border-transparent hover:cursor-pointer flex items-center`}
+              onClick={() => setCategory("wedding")}
+            >
+              Wedding
+            </span>
+            <span
+              className={`${
+                category === "traditionalWedding"
+                  ? "border-gray-600 dark:border-gray-500"
+                  : null
+              } py-1 border-b-2 border-transparent hover:cursor-pointer flex items-center`}
+              onClick={() => setCategory("traditionalWedding")}
+            >
+              Traditional Wedding
+            </span>
+            <span
+              className={`${
+                category === "birthday"
+                  ? "border-gray-600 dark:border-gray-500"
+                  : null
+              } py-1 border-b-2 border-transparent hover:cursor-pointer flex items-center`}
+              onClick={() => setCategory("birthday")}
+            >
+              Birthday
+            </span>
+            <span
+              className={`${
+                category === "convocation"
+                  ? "border-gray-600 dark:border-gray-500"
+                  : null
+              } py-1 border-b-2 border-transparent hover:cursor-pointer flex items-center`}
+              onClick={() => setCategory("convocation")}
+            >
+              Convocation
+            </span>
+            <span
+              className={`${
+                category === "family"
+                  ? "border-gray-600 dark:border-gray-500"
+                  : null
+              } py-1 border-b-2 border-transparent hover:cursor-pointer flex items-center`}
+              onClick={() => setCategory("family")}
+            >
+              Family
+            </span>
           </div>
         </div>
       </header>
+      <div><ImageCategories category={category} /></div>
     </div>
   );
 };
