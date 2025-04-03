4bef2211581e1fca1b3e01720fce5508fad14e6f
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-03 07:54:52 +0100

    Replace multiple divs with one component

---
+++ balinga-photography/src/app/data/miniPortfolio.js
@@ -0,0 +1,65 @@
+const data = [
+  {
+    id: 1,
+    img: ["/images/mini-portfolio/img-1.jpg"],
+    tag: "wedding",
+    href: "",
+  },
+  {
+    id: 2,
+    img: ["/images/mini-portfolio/img-2.jpg"],
+    tag: "wedding",
+    href: "",
+  },
+  {
+    id: 3,
+    img: ["/images/mini-portfolio/img-3.jpg"],
+    tag: "wedding",
+    href: "",
+  },
+  {
+    id: 4,
+    img: ["/images/mini-portfolio/img-4.jpg"],
+    tag: "birthday",
+    href: "",
+  },
+  {
+    id: 5,
+    img: ["/images/mini-portfolio/img-5.jpg"],
+    tag: "birthday",
+    href: "",
+  },
+  {
+    id: 6,
+    img: ["/images/mini-portfolio/img-6.jpg"],
+    tag: "birthday",
+    href: "",
+  },
+  {
+    id: 7,
+    img: ["/images/mini-portfolio/img-7.jpg"],
+    tag: "birthday",
+    href: "",
+  },
+  {
+    id: 8,
+    img: ["/images/mini-portfolio/img-8.jpg"],
+    tag: "birthday",
+    href: "",
+  },
+  {
+    id: 9,
+    img: ["/images/mini-portfolio/img-9.jpg"],
+    tag: "birthday",
+    href: "",
+  },
+  {
+    id: 9,
+    img: ["/images/mini-portfolio/img-10.jpg"],
+    tag: "birthday",
+    href: "",
+  },
+];
+
+// Export the object so it can be imported elsewhere
+export default data;
