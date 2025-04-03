3388e60ff80a421083783da9ea3327c0d1f68ced
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-03 07:52:31 +0100

    Add Connect component

--- balinga-photography/src/app/components/portfolio/Main.jsx
+++ balinga-photography/src/app/components/portfolio/Main.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-// Lazy load component
+// Lazy load components
 const Carousel = dynamic(() => import("../Carousel"), {
+  loading: () => (
+    <div className="w-full p-4 flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  ),
+  ssr: false,
+});
+const Categories = dynamic(() => import("./Categories"), {
+  loading: () => (
+    <div className="w-full p-4 flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  ),
+  ssr: false,
+});
+const Highlight = dynamic(() => import("../Highlight"), {
+  loading: () => (
+    <div className="w-full p-4 flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  ),
+  ssr: false,
+});
+const Connect = dynamic(() => import("./Connect"), {
   loading: () => (
     <div className="w-full p-4 flex justify-center items-center">
       <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
@@ -15,6 +39,13 @@ const Main = () => {
   return (
     <div className="bg-slate-200 dark:bg-black">
       <Carousel />
+      <div className="p-6 pb-0 md:p-10 md:pb-0 md:px-12 lg:px-24 mb-10 md:mb-16">
+        <Categories categoryType={"all"} />
+      </div>
+      <Connect />
+      <div className="p-6 md:p-10 md:pb-0 md:px-12 lg:px-24">
+        <Highlight />
+      </div>
     </div>
   );
 };
