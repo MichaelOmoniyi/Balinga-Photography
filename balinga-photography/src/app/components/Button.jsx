2dfb4aa71507640832b937d06cf6a5a348c69791
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-03 12:26:33 +0100

    Adding href prop to component

--- balinga-photography/src/app/components/Button.jsx
+++ balinga-photography/src/app/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Button = ({ text }) => {
+const Button = ({ text, href }) => {
   return (
-    <button className="transitionEffect flex justify-center items-center text-center py-2 px-8 text-sm border border-solid border-black rounded-3xl uppercase mx-auto mt-8 transition-all text-black hover:bg-black hover:text-white scale-110 opacity-0">
-      <a className="hover:text-white" href="/contact">
+    <button className="transitionEffect flex justify-center items-center text-center py-2 px-8 text-sm border border-solid border-black rounded-3xl uppercase mx-auto mt-8 transition-all bg-slate-50 text-black hover:bg-black hover:text-white scale-110 opacity-0">
+      <a className="hover:text-white" href={href}>
         {text}
       </a>
     </button>
