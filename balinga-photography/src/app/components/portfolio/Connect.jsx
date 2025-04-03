da1a144ad1056176f5ab21003dd5dadb8f792396
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-03 07:53:42 +0100

    Implement connect component

---
+++ balinga-photography/src/app/components/portfolio/Connect.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "../Button";
+import Image from "next/image";
+
+const Connect = () => {
+  return (
+    <div className="bg-slate-50 flex flex-col md:gap-6 lg:gap-10 md:grid md:grid-rows-2 grid-cols-2 p-6 md:p-0">
+      <div className="py-4 md:row-start-1 md:col-start-1 md:p-6 md:pr-0 lg:p-12 lg:pr-0">
+        <p className="font-mono text-base">
+          Let’s begin this journey together, where every step is a new
+          discovery.
+        </p>
+      </div>
+      <div className="w-full h-[calc(100vw)] md:h-auto md:row-span-2 md:col-start-2">
+        <Image
+          width={500}
+          height={500}
+          quality={100}
+          unoptimized
+          className="relative h-full w-full object-cover"
+          src="/images/connect-img.jpg"
+          alt=""
+        />
+      </div>
+      <div className="py-4 row-start-3 pb-4 md:row-start-2 md:col-start-1 md:p-6 md:pr-0 lg:p-12 lg:pr-0">
+        <h1 className="text-3xl font-bold">
+          Exploring the depths of human emotion through the art of photography.
+        </h1>
+        <Button text={"Let's Connect"} />
+      </div>
+    </div>
+  );
+};
+
+export default Connect;
