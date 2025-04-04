c269d8773bbe486c966f3c4badf98dde02294ced
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-04 20:16:45 +0100

    Create button component to be used in hero carousel component

---
+++ balinga-photography/src/app/components/Btn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Btn = ({ text, href }) => {
+  return (
+    <button className="transitionEffect flex justify-center items-center text-center py-2 px-8 text-sm border border-solid border-white rounded-3xl uppercase mx-auto mt-8 transition-all bg-transparent text-white hover:bg-white hover:text-black scale-110 opacity-0">
+      <a className="hover:text-white" href={href}>
+        {text}
+      </a>
+    </button>
+  );
+};
+
+export default Btn;
