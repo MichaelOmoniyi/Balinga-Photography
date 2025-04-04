5bd738c77183afa0388381f10e696d337ea1e34f
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-04 21:50:02 +0100

    Update page accessibility and cater for dark mode

--- balinga-photography/src/app/components/linksPage/Links.jsx
+++ balinga-photography/src/app/components/linksPage/Links.jsx
@@ -10,18 +10,18 @@ import {
 
 const Links = () => {
   return (
-    <div className="main-bg pl-4 pr-4 pb-16 lg:pl-40 lg:pr-40">
+    <div className="main-bg bg-slate-50 pl-4 pr-4 pb-16 lg:pl-40 lg:pr-40">
       <div className="heroImage absolute top-0 left-0 z-0">
         <div className="bg-darken w-full h-full bg-black opacity-45"></div>
       </div>
       <main>
         <div className="profile w-full relative z-10 flex flex-col justify-center items-center text-white pt-14">
-          <div className="profileImg w-28 h-28 rounded-full bg-gray-700 mb-4 lg:mb-10"></div>
+          <div className="profileImg w-28 h-28 rounded-full bg-slate-50 dark:bg-gray-700 mb-4 lg:mb-10"></div>
           <h1 className="text-3xl mb-1 lg:mb-3">Balinga</h1>
           <p>Photographer & Creator</p>
         </div>
-        <div className="sub-bg w-full relative flex flex-col justify-center items-center rounded-md mt-6 lg:mt-20 pt-6 pr-4 pb-24 pl-4">
-          <div className="w-full flex justify-around items-center gap-2 text-white pr-6 pl-6 md:pr-40 md:pl-40 lg:pr-80 lg:pl-80 md:pt-10 md:pb-10">
+        <div className="sub-bg bg-slate-100 shadow-lg w-full relative flex flex-col justify-center items-center rounded-md mt-6 lg:mt-20 pt-6 pr-4 pb-24 pl-4">
+          <div className="w-full flex justify-around items-center gap-2 text-gray-800 dark:text-white pr-6 pl-6 md:pr-40 md:pl-40 lg:pr-80 lg:pl-80 md:pt-10 md:pb-10">
             <a href="https://www.facebook.com/share/18jzqifpAY/">
               <i className="fab fa-facebook-f text-2xl hover:text-gray-400 transition duration-300"></i>
             </a>
@@ -37,7 +37,7 @@ const Links = () => {
             </a>
           </div>
 
-          <div className="links flex flex-col gap-4 md:gap-6 justify-center text-center text-xl text-white font-medium mt-6">
+          <div className="links flex flex-col gap-4 md:gap-6 justify-center text-center text-xl text-gray-800 dark:text-white font-medium mt-6">
             <a href="/links">BOOKING & ENQUIRY</a>
             {/* <a href="/links">EDITORIAL WEDDING SHOOT</a> */}
             {/* <a href="/links">MARCH CLASS</a> */}
