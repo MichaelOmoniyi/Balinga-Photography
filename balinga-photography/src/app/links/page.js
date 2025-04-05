02989225f5a81a6eba6efb211ae44ce44b768695
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-05 06:13:15 +0100

    Update page title

--- balinga-photography/src/app/links/page.js
+++ balinga-photography/src/app/links/page.js
@@ -1,14 +1,24 @@
+export const metadata = {
+  title: "Balinga Photography",
+  description: "Info about Balinga",
+};
+
 import React from "react";
-import Links from "../components/linksPage/Links";
+import dynamic from "next/dynamic";
 import "../components/linksPage/style.css"
+
+const Links = dynamic(() => import("../components/linksPage/Links"), {
+  loading: () => (
+    <div className="w-full p-4 flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  ),
+  ssr: false,
+});
 
 const page = () => {
   return (
     <>
-      <head>
-        <title>Links - Balinga Photography</title>
-        <meta name="description" content="Capturing your best moments" />
-      </head>
       <Links />
     </>
   );
