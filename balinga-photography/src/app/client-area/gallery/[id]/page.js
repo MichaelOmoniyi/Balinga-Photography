589591fbccd4c216dc5a223df0b0db7df8d113d8
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-05 05:15:02 +0100

    Resolve title not displaying issue

--- balinga-photography/src/app/client-area/gallery/[id]/page.js
+++ balinga-photography/src/app/client-area/gallery/[id]/page.js
@@ -1,34 +1,41 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Head from "next/head";
 import { useParams } from "next/navigation";
 import Portfolio from "@/app/components/portfolio/gallery/Portfolio";
 import "../../../components/portfolio/style.css";
 import data from "@/app/data/data";
 
-const page = () => {
+const Page = () => {
   const params = useParams();
   const { id } = params;
 
-  const [filteredData, setFilteredData] = useState([]);
-  useEffect(() => {
-      if (id) {
-        const numericId = Number(id);
-        const newFilteredData = data.filter((object) => object.id === numericId);
-        setFilteredData(newFilteredData);
-      }
-    }, [id]);
-  
+  const [filteredData, setFilteredData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <head>
-        <title>{`${filteredData[0]?.title}`}Balinga Photography</title>
-        <meta name="description" content="Capturing your best moments" />
-      </head>
-      <Portfolio id={id} />
-    </>
-  );
+  useEffect(() => {
+    if (id) {
+      const numericId = Number(id);
+      const newFilteredData = data.find((object) => object.id === numericId);
+      setFilteredData(newFilteredData);
+      setLoading(false);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (filteredData?.title) {
+      document.title = `${filteredData.title} - Balinga Photography`;
+    }
+  }, [filteredData]);
+
+  if (loading) {
+    return (
+      <div className="fixed top-0 left-0 w-screen h-screen bg-white flex justify-center items-center">
+        <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
+  return <Portfolio id={id} />;
 };
 
-export default page;
+export default Page;
