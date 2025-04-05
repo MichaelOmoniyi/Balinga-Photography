980460ea8053347d9dffb3b29aa42af5d68dbe6a
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-05 05:21:43 +0100

    Implementing other pages with 'Coming Soon' text

---
+++ balinga-photography/src/app/info/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center text-xl md:text-4xl text-gray-600 dark:text-gray-400'><h1>Coming Soon</h1></div>
+  )
+}
+
+export default page
