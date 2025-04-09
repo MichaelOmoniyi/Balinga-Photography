9c980594573bc19f3a5c7dd152a741eee307030d
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 17:03:02 +0100

    Modify component padding

--- balinga-photography/src/app/components/Highlight.jsx
+++ balinga-photography/src/app/components/Highlight.jsx
@@ -6,7 +6,7 @@ const Highlight = () => {
   useZoomEffect(".highlight");
 
   return (
-    <div className="w-full flex justify-center py-8">
+    <div className="w-full flex justify-center">
       <h4 className="highlight flex justify-center w-full md:w-5/12 text-white dark:bg-white dark:text-black font-bold text-5xl pb-12 pt-12 scale-110 opacity-0">
         @balinga
       </h4>
