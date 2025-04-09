5632add3a20cbc6ca38d6c09d60ace429718bc37
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 16:58:02 +0100

    Add json-like data containing some testimonials

---
+++ balinga-photography/src/app/data/miniTestimonials.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id: 1,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 2,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 3,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+];
+
+export default data;
