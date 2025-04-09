8d481831afd2b87d1c106ea2652e706e30911efb
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 16:58:12 +0100

    Add json-like data containing some testimonials

---
+++ balinga-photography/src/app/data/testimonials.js
@@ -0,0 +1,88 @@
+const data = [
+  {
+    id: 1,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 2,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 3,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 4,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 5,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 6,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 6",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 7,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 7",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 8,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 8",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 9,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 9",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 10,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 10",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 11,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 11",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+  {
+    id: 12,
+    img: "/images/img-sub.png",
+    title: "Tosin and T 12",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi cum dolore accusamus earum dolorum officia voluptatibus distinctio dolores et, amet quod, nulla ipsa iure molestias nostrum voluptatum, tempora quibusdam expedita enim.",
+  },
+];
+
+export default data;
