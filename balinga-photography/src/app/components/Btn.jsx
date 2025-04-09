0ea9c6024e425faa9f4763f206a9d227d77bf3dd
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 17:03:41 +0100

    Modify component padding

--- balinga-photography/src/app/components/Btn.jsx
+++ balinga-photography/src/app/components/Btn.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Btn = ({ text, href }) => {
   return (
-    <button className="transitionEffect flex justify-center items-center text-center py-2 px-8 text-sm border border-solid border-white rounded-3xl uppercase mx-auto mt-8 transition-all bg-transparent text-white hover:bg-white hover:text-black scale-110 opacity-0">
-      <a href={href}>
-        {text}
-      </a>
+    <button className="transitionEffect flex justify-center items-center text-center py-3 px-10 text-base md:text-lg border border-solid border-white rounded-3xl uppercase mx-auto mt-8 transition-all bg-transparent text-white hover:bg-white hover:text-black scale-110 opacity-0">
+      <a href={href}>{text}</a>
     </button>
   );
 };
