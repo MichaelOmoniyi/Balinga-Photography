cae72e636c298328aba86418cf0fb6ef5cbed31f
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 17:03:30 +0100

    Modify component padding and accessibility (dark-theme)

--- balinga-photography/src/app/components/Button.jsx
+++ balinga-photography/src/app/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ text, href }) => {
   return (
-    <button className="transitionEffect flex justify-center items-center text-center py-2 px-8 text-sm border border-solid border-black rounded-3xl uppercase mx-auto mt-8 transition-all bg-slate-50 text-black hover:bg-black hover:text-white scale-110 opacity-0">
+    <button className="transitionEffect flex justify-center items-center text-center py-3 px-10 text-base md:text-lg border border-solid border-black rounded-3xl uppercase mx-auto transition-all bg-slate-50 dark:bg-black text-black dark:text-slate-50 hover:bg-black dark:hover:bg-slate-50 hover:text-white dark:hover:text-black scale-110 opacity-0">
       <a className="hover:text-white" href={href}>
         {text}
       </a>
