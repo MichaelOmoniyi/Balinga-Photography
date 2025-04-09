705758c95fbd35404a19c72268ca0bd9aa43b690
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 16:50:18 +0100

    Modify border color

---
+++ balinga-photography/src/app/components/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Marquee = () => {
+
+  return (
+    <div className="w-full flex justify-center items-center bg-black dark:bg-white border border-solid  hover:bg-white dark:hover:bg-black hover:border-black dark:hover:border-white tr text-white hover:text-black dark:text-black dark:hover:text-white transition-all font-bold text-center md:text-lg py-2">
+      <marquee behavior="left" direction="slide">
+        <a href="/contact">NOW BOOKING 2025-2026 EVENTS! CLICK HERE TO GRAB</a>
+      </marquee>
+    </div>
+  );
+};
+
+export default Marquee;
