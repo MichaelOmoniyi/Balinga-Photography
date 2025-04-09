2625887b3dca2d1507e37499a788fe154c386b85
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 16:58:34 +0100

    Import main component into page

--- balinga-photography/src/app/testimonials/page.js
+++ balinga-photography/src/app/testimonials/page.js
@@ -1,8 +1,24 @@
+export const metadata = {
+  title: "Hear from our esteemed customers - Balinga Photography Prices",
+  description: "Testimonials",
+};
+
 import React from 'react'
+import dynamic from 'next/dynamic';
+import "../components/testimonials/style.css"
+
+const Main = dynamic(() => import("@/app/components/testimonials/Main"), {
+  loading: () => (
+    <div className="w-full p-4 flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  ),
+  ssr: false,
+});
 
 const page = () => {
   return (
-    <div>page</div>
+    <div><Main /></div>
   )
 }
 
