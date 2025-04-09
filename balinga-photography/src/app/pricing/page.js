6781765be1b2c96f085e53afc53f2fe87ba17898
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-09 16:58:49 +0100

    Import main Pricing component into page

--- balinga-photography/src/app/pricing/page.js
+++ balinga-photography/src/app/pricing/page.js
@@ -1,9 +1,27 @@
-import React from 'react'
+export const metadata = {
+  title: "Affordable Photographer - Balinga Photography Prices",
+  description: "Photography Prices",
+};
+
+import React from "react";
+import dynamic from "next/dynamic";
+import "../components/pricing/style.css"
+
+const Pricing = dynamic(() => import("@/app/components/pricing/Main"), {
+  loading: () => (
+    <div className="w-full p-4 flex justify-center items-center">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  ),
+  ssr: false,
+});
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <Pricing />
+    </div>
+  );
+};
 
-export default page
+export default page;
