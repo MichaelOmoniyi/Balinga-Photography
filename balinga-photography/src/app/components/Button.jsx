26bf9c19f0eaaf4b9e6f854aafc5516136e4a505
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-13 18:42:28 +0100

    remove class from 'a' tag

--- balinga-photography/src/app/components/Button.jsx
+++ balinga-photography/src/app/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ text, href }) => {
   return (
     <button className="transitionEffect flex justify-center items-center text-center py-3 px-10 text-base md:text-lg border border-solid border-black rounded-3xl uppercase mx-auto transition-all bg-slate-50 dark:bg-black text-black dark:text-slate-50 hover:bg-black dark:hover:bg-slate-50 hover:text-white dark:hover:text-black scale-110 opacity-0">
-      <a className="hover:text-white" href={href}>
+      <a href={href}>
         {text}
       </a>
     </button>
