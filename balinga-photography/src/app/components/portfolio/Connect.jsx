1427efbbbc7bd62ff612318c2d461bcb0a6324c6
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-13 18:38:55 +0100

    Modify padding

--- balinga-photography/src/app/components/portfolio/Connect.jsx
+++ balinga-photography/src/app/components/portfolio/Connect.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Button from "../Button";
+import Button from "./Button";
 import Image from "next/image";
 
 const Connect = () => {
   return (
     <div className="bg-slate-50 flex flex-col md:gap-6 lg:gap-10 md:grid md:grid-rows-2 grid-cols-2 mt-10 md:mt-16 mb-0">
-      <div className="py-4 md:row-start-1 md:col-start-1 md:p-6 md:pl-12 md:pr-0">
+      <div className="p-4 py-4 md:row-start-1 md:col-start-1 md:p-6 md:pl-12 md:pr-0">
         <p className="font-mono text-base">
           Let’s begin this journey together, where every step is a new
           discovery.
@@ -22,7 +22,7 @@ const Connect = () => {
           alt="Family photograph"
         />
       </div>
-      <div className="py-4 row-start-3 pb-4 md:row-start-2 md:col-start-1 md:p-6 md:pl-12 md:pr-0">
+      <div className="p-4 py-4 row-start-3 pb-4 md:row-start-2 md:col-start-1 md:p-6 md:pl-12 md:pr-0">
         <h1 className="text-3xl font-bold mb-4">
           Exploring the depths of human emotion through the art of photography.
         </h1>
