a5256dae51cb2c8f099f7b6af932bb0759c4d8b0
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-25 15:15:24 +0100

    Optimizing background images

--- balinga-photography/src/app/components/home/Connect.jsx
+++ balinga-photography/src/app/components/home/Connect.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import useFadeIn from "@/app/Hooks/useFadeIn";
+import OptimizedImage from "../OptimizedImage";
 
 const Connect = () => {
   useFadeIn("#connect-1, #connect-2, #connect-3");
@@ -8,9 +9,19 @@ const Connect = () => {
   return (
     <div>
       <section className="section connect">
-        <div className="connect-bg">
-          <div className="overlay"></div>
-          <div className="connect-content">
+        <div className="w-full h-full relative">
+          <OptimizedImage
+            src="/images/connect-img.jpg"
+            alt="Connect Background"
+            width={1920}
+            height={1080}
+            quality={75}
+            priority={true}
+            sizes="100vw"
+            className="absolute top-0 left-0 inset-0 w-full h-[95vh] md:h-[70vh] lg-[100vh] object-cover"
+          />
+          <div className="overlay absolute inset-0 bg-black opacity-60 z-10"></div>
+          <div className="connect-content relative z-20">
             <h1
               className="text-5xl font-bold text-white text-center animatable animatable--large-title animatable--animated animatable--finished"
               id="connect-1"
@@ -26,7 +37,7 @@ const Connect = () => {
             </h1>
             <div className="mt-6">
               <h5
-                className="text-lg font-medium text-gray-100 text-center"
+                className="flex justify-center gap-2 text-lg font-medium text-gray-100 text-center"
                 id="connect-2"
               >
                 <span className="inline-block">Portrait</span>
