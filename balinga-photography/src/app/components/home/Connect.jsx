edd720d17e35f75bc34ec15e27e05bc3eaae06df
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2025-04-25 23:31:37 +0100

    Optimize images

--- balinga-photography/src/app/components/home/Connect.jsx
+++ balinga-photography/src/app/components/home/Connect.jsx
@@ -18,7 +18,7 @@ const Connect = () => {
             quality={75}
             priority={true}
             sizes="100vw"
-            className="absolute top-0 left-0 inset-0 w-full h-[95vh] md:h-[70vh] lg-[100vh] object-cover"
+            className="absolute top-0 left-0 inset-0 w-full h-[95vh] md:h-[70vh] lg:h-[100vh] object-cover"
           />
           <div className="overlay absolute inset-0 bg-black opacity-60 z-10"></div>
           <div className="connect-content relative z-20">
